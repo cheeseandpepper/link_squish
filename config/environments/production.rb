db7be752d2303ad82cb43c0b05da197e39c6ce87
Mike Lerner <cheeseandpepper@gmail.com>
2019-03-17 13:07:18 -0700

    more actioncable config for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,9 +41,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
+  config.action_cable.mount_path = '/cable'
   config.action_cable.url = 'wss://link-squish.herokuapp.com/cable' 
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
   #config.web_socket_server_url = "wss://link-squish.herokuapp.com/cable" 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
